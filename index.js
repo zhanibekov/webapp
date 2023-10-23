59d4928b8447ac38b054dc7d105dad80628d22d3
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-10-23 20:22:44 +0600

    added library nodemon

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+    res.send(' Hello world!!!!');
+});
+
+app.listen(4444, (err) => {
+    if (err) {
+        return console.log(err);
+    }
+    console.log('Server is working');
+});
