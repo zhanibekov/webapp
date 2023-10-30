7c87701b750a167ecb0d81639ac4cedf51017463
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-10-30 19:23:03 +0600

    i created User.Model

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import mongoose, { connect } from 'mongoose';
+
+mongoose.
+connect('mongodb+srv://elamanzhanibekov:2006@cluster0.oqzqyvs.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => console.log('DB is OK!'))
+    .catch((err) => console.log('DB is error', err));
 
 const app = express();
 app.use(express.json());
