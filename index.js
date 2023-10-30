1534e87476940cecae006dfd889599c6ebb91b36
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-10-31 00:00:26 +0600

    I installed express validator and connected it to my project

--- index.js
+++ index.js
@@ -5,26 +5,14 @@ import mongoose, { connect } from 'mongoose';
 mongoose.
 connect('mongodb+srv://elamanzhanibekov:2006@cluster0.oqzqyvs.mongodb.net/?retryWrites=true&w=majority')
     .then(() => console.log('DB is OK!'))
-    .catch((err) => console.log('DB is error', err));
+    .catch((err) => console.log('DB IS ERROR', err))
 
 const app = express();
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.send(' Hello world!!!!');
-});
+
 
 app.post('/auth/login', (req, res) => {
-    console.log(req.body);
-    const token = jwt.sign({
-            fullName: "Zhunisali Shanabek",
-        },
-        'secret123',
-    );
-    res.json({
-        email: req.body.email,
-        success: true,
-        token,
-    });
+
 })
 
 app.listen(4444, (err) => {
