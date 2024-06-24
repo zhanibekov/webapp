1decce8e55f671e591b7d6f09ba54be299804fd7
yelaman <67456840+zhanibekov@users.noreply.github.com>
2024-06-24 15:03:54 +0500

    Update index.js

--- index.js
+++ index.js
@@ -50,24 +50,11 @@ app.post('/posts', checkAuth, handleValidationErrors, postCreateValidation, Post
 app.delete('/posts/:id', PostController.remove);
 app.patch('/posts/:id', checkAuth, handleValidationErrors, postCreateValidation, PostController.update);
 
-app.get('/', (req, res) => {
+app.get('/posts', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.get('/file', async(req, res) => {
-    try {
-        const data = await fs.promises.readFile(path.join(__dirname, 'file.txt'), 'utf8');
-        res.send(data);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-
-app.listen(process.env.PORT || 4444, (err) => {
-    if (err) {
-        return console.log(err);
-    }
-    console.log('Server is working');
+const PORT = process.env.PORT || 4444;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
